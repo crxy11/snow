720bdac26c1ea439c9ad703d8eca5f1263c24278
Jan Lagarden <jan.lagarden@live.de>
2015-12-08 21:11:50 +0100

    Initial Commit

---
+++ snow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  snow
+//
+//  Created by Jan Lagarden on 08.12.15.
+//  Copyright © 2015 Jan Lagarden. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
